904fb1caf575754d4b6d200ef8aba17bd574948f
Sahyadri-1674 <117518329+Sahyadri-1674@users.noreply.github.com>
2022-11-05 22:56:52 +0530

    Update FileHandling2.java

--- FileHandling2.java
+++ FileHandling2.java
@@ -4,9 +4,9 @@ import java.util.*;
 class FileHandling2{
 	public static void main(String [] args) throws IOException{
 		int i;
-		FileOutputStream fout = new FileOutputStream("C:/Prasadjava/name1.txt",true);//If the doesn't exists it will create it
+		FileOutputStream fout = new FileOutputStream("C:/Prasadjava/name1.txt",true);  //If the doesn't exists it will create it
 		String s = "Prasad Arote";
-		char ch[] = s.toCharArray();
+		char ch[] = s.toCharArray();  //Converts string into character Array.
 		for(i=0;i<s.length();i++)
 			fout.write(ch[i]);
 		fout.close();
