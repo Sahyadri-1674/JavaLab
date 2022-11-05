1f8bdff2684ed8afa755563892b3a2a35a3bae22
Sahyadri-1674 <117518329+Sahyadri-1674@users.noreply.github.com>
2022-11-05 22:55:52 +0530

    Add files via upload

---
+++ FileHandling2.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+class FileHandling2{
+	public static void main(String [] args) throws IOException{
+		int i;
+		FileOutputStream fout = new FileOutputStream("C:/Prasadjava/name1.txt",true);//If the doesn't exists it will create it
+		String s = "Prasad Arote";
+		char ch[] = s.toCharArray();
+		for(i=0;i<s.length();i++)
+			fout.write(ch[i]);
+		fout.close();
+	}
+}
